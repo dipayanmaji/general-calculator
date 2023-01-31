5e2b34636c7696b2df0256774c2f64d7116b1530
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-01-31 15:49:04 +0530

    javaScript code done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+const display = document.getElementById("ansDisplay");
+function clicked(event){
+    display.value += event.target.value;
+}
+function clickedAns(){
+    display.value = eval(display.value);
+}
+function resetClicked(){
+    display.value = null;
+}
